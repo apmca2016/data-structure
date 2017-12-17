e02640b1cfb0d3b21b34968946f38ae2c1bbc5c7
pandeyanandap <anandpandey.ap@gmail.com>
2017-12-17 17:54:22 +0530

    First commit
    uploading all  file

---
+++ CalculatorApp/src/com/ecs/anand/MyCalculators.java
@@ -0,0 +1,14 @@
+package com.ecs.anand;
+
+public class MyCalculators {
+
+	public double add(double num1,double num2)
+	{
+		return num1 + num2;
+	}
+	
+	public double sub(double num1,double num2)
+	{
+		return num1 - num2;
+	}
+}
---
+++ CalculatorApp/src/com/ecs/anand/Test.java
@@ -0,0 +1,11 @@
+package com.ecs.anand;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		MyCalculators m1= new MyCalculators();
+		System.out.println("Add: " + m1.add(8, 9));
+		System.out.println("Sub: " + m1.sub(22, 12));
+	}
+}
